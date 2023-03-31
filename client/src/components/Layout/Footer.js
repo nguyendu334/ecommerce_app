1773065e3869e8486738a97337656f2c9b7d63bd
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-31 13:33:36 +0700

    React setup

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="bg-dark text-light p-3">
+            <h4 className="text-center">All right Reserved &copy; haha193</h4>
+        </div>
+    );
+};
+
+export default Footer;
