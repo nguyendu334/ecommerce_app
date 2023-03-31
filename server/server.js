8435e6b88920eedaa2e6d40649d3aa2de7042bed
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-31 08:50:44 +0700

    Server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
