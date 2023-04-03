f8bfcaa7e67c0d7cc87d819a1a35afa12e6d24a9
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-04-03 08:53:08 +0700

    Context Api setup

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import Layout from '../../components/Layout/Layout';
+import { useAuth } from '../../context/auth';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [auth, setAuth] = useAuth();
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -16,8 +18,14 @@ const Login = () => {
                 email,
                 password,
             });
-            if(res && res.data.success) {
+            if (res && res.data.success) {
                 toast.success(res.data && res.data.message);
+                setAuth({
+                    ...auth,
+                    user: res.data.user,
+                    token: res.data.token,
+                });
+                localStorage.setItem('auth', JSON.stringify(res.data));
                 navigate('/');
             } else {
                 toast.error(res.data.message);
